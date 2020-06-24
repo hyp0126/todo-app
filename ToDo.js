f63cdc71cf160b46959716c33dd74450d9aaec88
YHong <hyp0126@gmail.com>
2020-06-24 01:39:04 -0400

    Saving To Dos, Saving Changes to the Disk

--- ToDo.js
+++ ToDo.js
@@ -7,18 +7,31 @@ import {
   Dimensions,
   TextInput,
 } from "react-native";
+import PropTypes from "prop-types";
 
 const { width, height } = Dimensions.get("window");
 
 export default class ToDo extends Component {
-  state = {
-    isEditing: false,
-    isCompleted: false,
-    toDoValue: "",
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEditing: false,
+      toDoValue: props.text,
+    };
+  }
+  static propTypes = {
+    text: PropTypes.string.isRequired,
+    isCompleted: PropTypes.bool.isRequired,
+    deleteToDo: PropTypes.func.isRequired,
+    id: PropTypes.string.isRequired,
+    uncompleteToDo: PropTypes.func.isRequired,
+    completeToDo: PropTypes.func.isRequired,
+    updateToDo: PropTypes.func.isRequired,
   };
+
   render() {
-    const { isCompleted, isEditing, toDoValue } = this.state;
-    const { text } = this.props;
+    const { isEditing, toDoValue } = this.state;
+    const { text, id, deleteToDo, isCompleted } = this.props;
     return (
       <View style={styles.container}>
         <View style={styles.column}>
@@ -42,6 +55,7 @@ export default class ToDo extends Component {
               onChangeText={this._controlInput}
               returnKeyType={"done"}
               onBlur={this._finishEditing}
+              underlineColorAndroid={"transparent"}
             />
           ) : (
             <Text
@@ -69,7 +83,12 @@ export default class ToDo extends Component {
                 <Text style={styles.actionText}>✏️</Text>
               </View>
             </TouchableOpacity>
-            <TouchableOpacity>
+            <TouchableOpacity
+              onPressOut={(event) => {
+                event.stopPropagation();
+                deleteToDo(id);
+              }}
+            >
               <View style={styles.actionContainer}>
                 <Text style={styles.actionText}>❌</Text>
               </View>
@@ -79,21 +98,26 @@ export default class ToDo extends Component {
       </View>
     );
   }
-  _toogleComplete = () => {
-    this.setState((prevState) => {
-      return {
-        isCompleted: !prevState.isCompleted,
-      };
-    });
+  _toogleComplete = (event) => {
+    event.stopPropagation();
+    const { isCompleted, uncompleteToDo, completeToDo, id } = this.props;
+    if (isCompleted) {
+      uncompleteToDo(id);
+    } else {
+      completeToDo(id);
+    }
   };
-  _startEditing = () => {
-    const { text } = this.props;
+  _startEditing = (event) => {
+    event.stopPropagation();
     this.setState({
       isEditing: true,
-      toDoValue: text,
     });
   };
-  _finishEditing = () => {
+  _finishEditing = (event) => {
+    event.stopPropagation();
+    const { toDoValue } = this.state;
+    const { id, updateToDo } = this.props;
+    updateToDo(id, toDoValue);
     this.setState({
       isEditing: false,
     });
@@ -143,7 +167,6 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     width: width / 2,
-    justifyContent: "space-between",
   },
   actions: {
     flexDirection: "row",
